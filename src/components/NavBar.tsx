116a7fde06c53f0f447221293c5bf22a8e3c7add
Orlando Murcio <ato.murcio@gmail.com>
2023-06-09 21:56:05 -0600

    feat: adjust navbar hight

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 import { Box, Center, Text } from '@chakra-ui/react';
 
 export const NavBar = () => (
-  <Box bg="blue.300" height={100} width="100%">
-    <Center bg="tomato" h={['70px']} color="white">
+  <Box bg="blue.300" width="100%">
+    <Center bg="tomato" h="120px" color="white">
       <Text fontSize={['14px', '25px', '30px', '50']}>
         DayCare Managment System
       </Text>
